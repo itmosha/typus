c1f04f72acbb06219a012a8d022afda47f65f51b
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-04-01 19:41:38 +0300

    feat: switched to React...

---
+++ typus_frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
