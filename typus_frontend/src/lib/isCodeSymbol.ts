73c04936d3f737cca6c63724519d760d21bc30b0
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-04-05 01:11:07 +0300

    feat: isCodeSymbol func documented

--- typus_frontend/src/lib/isCodeSymbol.ts
+++ typus_frontend/src/lib/isCodeSymbol.ts
@@ -1,5 +1,11 @@
 
-
+/**
+ * Determines if a character is a valid symbol for putting it into the code field.
+ * 
+ * @param {string} c - The character which need to be checked. 
+ * @returns {boolean} - True for valid and false for invalid characters.
+ * 
+ */
 const isCodeSymbol = (c: string): boolean => {
     if (c.length !== 1) return false;
     if (c === '[' || c === ']') return true;
