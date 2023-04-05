599cbaabb52d07fd2b67f109a163bf4c7a8a60b1
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-04-05 15:39:28 +0300

    feat: react router setup

--- typus_frontend/src/index.tsx
+++ typus_frontend/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import IndexPage from './pages/IndexPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <IndexPage/>
+  },
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <IndexPage />
+  <RouterProvider router={router} />
 );
