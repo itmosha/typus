c433b3f672eb2813b1e21895bf2ab9633d0d5232
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-04-09 17:14:35 +0300

    feat: foreign key referencing language slug instead of id

--- typus_backend/migrations/20230409105607_create_table_code_samples.up.sql
+++ typus_backend/migrations/20230409105607_create_table_code_samples.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE code_samples (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
-    lang_id BIGINT NOT NULL,
-    CONSTRAINT fk_lang FOREIGN KEY(lang_id) REFERENCES programming_languages(id)
+    lang_slug CHAR(20) NOT NULL,
+    CONSTRAINT fk_lang FOREIGN KEY(lang_slug) REFERENCES programming_languages(slug)
 );
