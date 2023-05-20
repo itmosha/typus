f7ee1da6dc7f8432ff7b19606e8979661420c2ec
itmosha <itmosha0@gmail.com>
2023-05-21 00:16:37 +0300

    feat: Add functions to check for user roles
    - Create IsUser, IsModerator and IsAdmin functions
    - Add comments for IsAuth function

--- typus_backend/internal/middleware/user_role.go
+++ typus_backend/internal/middleware/user_role.go
@@ -7,8 +7,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Check if a user is at least registered.
+func IsUser() gin.HandlerFunc {
+	return HasRole(models.USER)
+}
+
+// Check if a user is at least a moderator.
+func IsModerator() gin.HandlerFunc {
+	return HasRole(models.MODERATOR)
+}
+
+// Check if a user is admin.
+func IsAdmin() gin.HandlerFunc {
+	return HasRole(models.ADMIN)
+}
+
+// Check if a user has a certain role.
 func HasRole(requredRole models.ROLE) gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		// Get token from gin's context
 		token, _ := c.Get("token")
 
 		k := token.(*jwt_funcs.JWTClaims)
@@ -17,6 +35,7 @@ func HasRole(requredRole models.ROLE) gin.HandlerFunc {
 			return
 		}
 
+		// Check if user has the role
 		if t.Role < requiredRole {
 			c.AbortWithStatusJSON(http.StatusForbidden, map[string]string{"message": "You do not have access to this data"})
 			return
